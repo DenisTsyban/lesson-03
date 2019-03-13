b50674fc7c427b5e3c40667ba3b6b8f4e324f07c
DenisTsyban <47360875+DenisTsyban@users.noreply.github.com>
2019-03-13 10:11:09 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Homework 1
-
-function foo () {
-  console.log("Переданные аргументы: " + arguments[0] + ", " + arguments[1] + ", " + arguments[2]);
-}
- 
-foo(10, false, "google");
-VM1591:2 Переданные аргументы: 10, false, google
